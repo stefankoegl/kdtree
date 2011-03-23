ada5842d829efb4497df5c75ab5d711a211e4f1e
Stefan Kögl <stefan@skoegl.net>
2011-03-23 10:09:42 +0200

    allow adding points to a kdtree

--- kdtree/kdtree.py
+++ kdtree/kdtree.py
@@ -5,16 +5,54 @@ class Node(object):
         self.left_child = left_child
         self.right_child = right_child
 
+
+    def add(self, point, depth=0):
+        """
+        Adds a point to the current node or recursively
+        descends to one of its children.
+
+        Users should call add() only to the topmost tree node or
+        pass the correct depth if they call for deeper nodes.
+        """
+
+        if self.location is None:
+            self.location = point
+            return
+
+        dim = check_dimensionality([self.location, point])
+        axis = select_axis(dim, depth)
+
+        if point[axis] < self.location[axis]:
+            if self.left_child is None:
+                self.left_child = Node(point, None, None)
+            else:
+                self.left_child.add(point, depth+1)
+
+        else:
+            if self.right_child is None:
+                self.right_child = Node(point, None, None)
+            else:
+                self.right_child.add(point, depth+1)
+
+
     def __repr__(self):
         return '<Node at %s>' % repr(self.location)
 
+
+
+def select_axis(dimensions, depth):
+    """
+    Select axis by cycling through them
+    """
+    return depth % dimensions
+
+
 def create(point_list=[], depth=0):
     if not point_list:
-        Node(None, None, None)
+        return Node(None, None, None)
 
-    # Select axis by cycling through them
     dim = check_dimensionality(point_list)
-    axis = depth % dim
+    axis = select_axis(dim, depth)
 
     # Sort point list and choose median as pivot element
     point_list.sort(key=lambda point: point[axis])
