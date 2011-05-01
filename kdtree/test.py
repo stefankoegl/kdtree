3657425f96213e2f622d5792941cd66e883d9dac
Stefan Kögl <stefan@skoegl.net>
2011-05-01 22:46:18 +0300

    specify range for random tree

--- kdtree/test.py
+++ kdtree/test.py
@@ -1,19 +1,19 @@
 from __future__ import absolute_import
 
-from random import random
+import random
 from itertools import islice
 
 import kdtree
 
 
-def random_tree(nodes=20, dimensions=3, scale=100):
+def random_tree(nodes=20, dimensions=3, minval=0, maxval=100):
     points = list(islice(random_points(), 0, nodes))
     tree = kdtree.create(points)
     return tree
 
-def random_point(dimensions, scale):
-    return tuple(int(random() * scale) for _ in range(dimensions))
+def random_point(dimensions, minval, maxval):
+    return tuple(random.randint(minval, maxval) for _ in range(dimensions))
 
-def random_points(dimensions=3, scale=100):
+def random_points(dimensions=3, minval=0, maxval=100):
     while True:
-        yield random_point(dimensions, scale)
+        yield random_point(dimensions, minval, maxval)
