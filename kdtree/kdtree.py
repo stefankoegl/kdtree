c1321bb1c8d98a932f5a6d39a6506833ea500722
Stefan Kögl <stefan@skoegl.net>
2011-05-04 19:52:49 +0300

    simplify KDNode.rebalance()

--- kdtree/kdtree.py
+++ kdtree/kdtree.py
@@ -303,7 +303,7 @@ class KDNode(Node):
         """
         Returns the (possibly new) root of the rebalanced tree
         """
-        return create(list([x.data for x in self.inorder()]))
+        return create([x.data for x in self.inorder()])
 
 
     def axis_dist(self, point, axis):
