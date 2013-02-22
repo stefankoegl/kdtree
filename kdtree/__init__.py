cd563afaaddbf758603110591a4e42804e2af1d8
Stefan Kögl <stefan@skoegl.net>
2013-02-22 08:02:50 +0100

    bump version to 0.3

--- kdtree/__init__.py
+++ kdtree/__init__.py
@@ -7,6 +7,6 @@ https://en.wikipedia.org/wiki/K-d_tree
 """
 
 __author__ = 'Stefan Kögl <stefan@skoegl.net>'
-__version__ = '0.2'
+__version__ = '0.3'
 __website__ = 'https://github.com/stefankoegl/kdtree'
 __license__ = 'GNU General Public License v3 or later'
